3e1240102c32fc351c6ee1b03eeb272e79a17b4d
mehedi <mehedihasan712277@gmail.com>
2026-04-18 17:34:25 +0600

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
 import AllSurahList from "@/components/ui/home/AllSurahList";
 import Hero from "@/components/ui/home/Hero";
+import logo from "@/assets/quran.png";
+import Image from "next/image";
 
 const page = () => {
     return (
-        <div className="space-y-4">
+        <div className="space-y-12">
             <Hero></Hero>
             <AllSurahList classes="grid grid-cols-1 sm:grid-cols-2 gap-2.5 max-w-5xl mx-auto px-4 xl:px-0"></AllSurahList>
+            <footer className="border-t border-border flex flex-col items-center gap-4 py-20">
+                <div className="space-y-1">
+                    <div className="flex gap-2">
+                        <Image src={logo} width={35} height={35} alt="logo" />
+                        <p className="text-3xl font-bold uppercase">القرآن</p>
+                    </div>
+                    <p className="text-text-muted uppercase text-sm tracking-wider">quran mazid</p>
+                </div>
+                <p>Your daily Quran companion</p>
+            </footer>
         </div>
     );
 };
